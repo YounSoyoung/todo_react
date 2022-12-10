95b201cabd5d50f807594e29b9e8cec0b99ba36d
YounSoyoung <grace902853@gmail.com>
2022-12-10 15:52:30 +0900

    Todo 삭제기능

--- src/App.js
+++ src/App.js
@@ -1,40 +1,73 @@
+import React, {useState} from 'react';
+import './css/main.css';
 import logo from './logo.svg';
 import './App.css';
 import Todo from './components/Todo';
-import {List} from '@mui/material'
-import TodoList from '../../ajax-basic/src/TodoList';
+import {List} from '@mui/material';
+import AddTodo from './components/AddTodo';
 
-//화면에 렌더링할 할 일 데이터
-const items = [
-  {
-      id: 1,
-      title: '점심 메뉴 고르기',
-      done: true
-  },
-  {
-      id: 2,
-      title: '책 읽기',
-      done: false
-  },
-  {
-    id: 3,
-    title: '동영상 강의 보기',
-    done: false
-  }
-];
-
-const todoItems = items.map(item => {
-  return <Todo key={item.id} item={item}/>;
-});
 
 const App = () => {
+
+  const [itemList, setItemList] = useState(//[
+  //   {
+  //       id: 1,
+  //       title: '점심 메뉴 고르기',
+  //       done: true
+  //   },
+  //   {
+  //       id: 2,
+  //       title: '책 읽기',
+  //       done: false
+  //   },
+  //   {
+  //     id: 3,
+  //     title: '동영상 강의 보기',
+  //     done: false
+  //   }
+  // ]
+  );
+
+  
+  //AddTodo에게 전달할 함수
+  //할 일 추가 처리 함수
+  const add = (item) => {
+    item.id = itemList.length + 1;
+    item.done = false;
+    
+    // console.log('add 호출됨!');
+    // console.log(item);
+    
+    setItemList(itemList => itemList.concat(item));
+  };
+  
+  //Todo에게 보낼 삭제 함수
+  //target: 내가 삭제할 객체, item: 배열에 저장된 객체
+  const remove = target => {
+    console.log('remove call!!');
+
+    const filteredItemList = itemList.filter(item => target.id !== item.id); //아이디가 같을 경우 배열에 저장되지 않으므로 삭제된다.
+
+    setItemList(filteredItemList);
+  };
+  
+  const todoItems = itemList.map(item => <Todo key={item.id} item={item} remove={remove}/>);
+
+
+//   useEffect(() => {
+//     // console.log('재 렌더링!!');
+//     // console.log(itemList);
+// }, [itemList]);
+
   return (
-    <div className="App">
+    <div className="wrapper">
+      <AddTodo add={add} />
       <List>
-        {todoItems}
+          {todoItems}
       </List>
     </div>
   );
-}
+};
+
 
 export default App;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,23 @@
 import React, {useState} from 'react';
 
 //https://mui.com/material-ui/getting-started/learn/
-import {ListItem, ListItemText, InputBase, Checkbox} from "@mui/material";
+import {ListItem, ListItemText, InputBase, Checkbox, ListItemSecondaryAction, IconButton} from "@mui/material";
+import { DeleteOutline } from '@mui/icons-material';
 
 
              //매개변수로 props를 넣어 App 컴포넌트에 있는 item 값을 Todo 컴포넌트에서 사용할 수 있다.
              //props 대신 중괄호를 넣으면 props.item이라고 할 필요가 없다.
-const Todo = ({ item }) => {
+const Todo = ({ item, remove }) => {
 
     console.log(item);
 
     const {id, title, done} = item;
+
+    //삭제 이벤트 핸들러
+    const removeHandler = e => {
+        // console.log(item);
+        remove(item);
+    }
 
     return (
         <ListItem>
@@ -26,6 +33,12 @@ const Todo = ({ item }) => {
                     fullWidth={true}
                 />
             </ListItemText>
+            {/* 삭제 버튼 */}
+            <ListItemSecondaryAction>
+                <IconButton aria-label="Delete Todo" onClick={removeHandler}>
+                    <DeleteOutline/>
+                </IconButton>
+            </ListItemSecondaryAction>
         </ListItem>
     );
 }
